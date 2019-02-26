7744e77db8e4bcfabc25376499ddc33d94f4749d
WiltencirDG <wiltencir@gmail.com>
2019-02-26 09:25:31 -0300

    MenuPass

---
+++ php/Menu/menuPass.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang=''>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="../../css/styles.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <title>CSS MenuMaker</title>
+</head>
+<body>
+
+<div id='cssmenu'>
+<ul>
+   <li class='active'><a href='#'><span>Perfil</span></a></li>
+   <li><a href='#'><span>Destino</span></a></li>
+   <li><a href='#'><span>Checklist</span></a></li>
+   <li class='last'><a href='#'><span>Sair</span></a></li>
+</ul>
+</div>
+</body>
+<html>
