e3187b954b9f8e62bdc081d5c7b58e5228f14614
WiltencirDG <wiltencir@gmail.com>
2019-02-26 10:18:51 -0300

    Removed menupass

--- php/Menu/menuPass.php
+++ php/Menu/menuPass.php
@@ -1,22 +1,12 @@
-<!doctype html>
-<html lang=''>
-<head>
-   <meta charset='utf-8'>
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="../../css/styles.css">
-   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-   <title>CSS MenuMaker</title>
-</head>
+<!DOCTYPE html>
+<html>
+<header>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</header>
 <body>
 
-<div id='cssmenu'>
-<ul>
-   <li class='active'><a href='#'><span>Perfil</span></a></li>
-   <li><a href='#'><span>Destino</span></a></li>
-   <li><a href='#'><span>Checklist</span></a></li>
-   <li class='last'><a href='#'><span>Sair</span></a></li>
-</ul>
-</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 </body>
-<html>
+</html> 
